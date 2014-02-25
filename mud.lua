784fafa75a48fc105f7608fddccb3987a23eca70
Carlos Manuel Escalona Villeda <cxescalona@gmail.com>
2014-02-25 12:35:09 -0600

    primeras pruebas

--- mud.lua
+++ mud.lua
@@ -1,14 +1,5 @@
-function on_msg_receive (msg)
-vardump (msg)
-end
-
-function parseMessage (msg)
-print(msg)
-end
-
-function on_our_id (id)
-  our_id = id
-end
+started = 0
+our_id = 0
 
 function vardump(value, depth, key)
   local linePrefix = ""
@@ -45,4 +36,60 @@ function vardump(value, depth, key)
   else
     print(spaces..linePrefix.."("..type(value)..") "..tostring(value))
   end
+end
+
+print ("HI, this is lua script")
+
+
+
+function on_msg_receive (msg)
+  if started == 0 then
+    return
+  end
+  if msg.out then
+    return
+  end
+  if (msg.text == 'ping') then
+    if (msg.to.id == our_id) then
+      print ('sending pong to ' .. tostring (msg.from.print_name))
+      send_msg (msg.from.print_name, 'pong')
+    else
+      print ('sending pong to ' .. tostring (msg.to.print_name))
+      send_msg (msg.to.print_name, 'pong')
+    end
+    return
+  end
+  if (msg.text == 'PING') then
+    if (msg.to.id == our_id) then
+      fwd_msg (msg.from.print_name, msg.id)
+    else
+      fwd_msg (msg.to.print_name, msg.id)
+    end
+    return
+  end
+  --vardump (msg)
+  --print ( "Message # " .. msg.id .. " (flags " .. msg.flags .. ")")
+end
+
+function on_our_id (id)
+  our_id = id
+end
+
+function on_secret_chat_created (peer)
+  --vardump (peer)
+end
+
+function on_user_update (user)
+  --vardump (user)
+end
+
+function on_chat_update (user)
+  --vardump (user)
+end
+
+function on_get_difference_end ()
+end
+
+function on_binlog_replay_end ()
+  started = 1
 end
