a09c465a9bd88c12a953e894bccd8382fb59b9ff
Carlos Manuel Escalona Villeda <cxescalona@gmail.com>
2014-02-25 12:29:56 -0600

    primeras pruebas

--- mud.lua
+++ mud.lua
@@ -1,1 +1,48 @@
-print("holaaa")
+function on_msg_receive (msg)
+vardump (msg)
+end
+
+function parseMessage (msg)
+print(msg)
+end
+
+function on_our_id (id)
+  our_id = id
+end
+
+function vardump(value, depth, key)
+  local linePrefix = ""
+  local spaces = ""
+  
+  if key ~= nil then
+    linePrefix = "["..key.."] = "
+  end
+  
+  if depth == nil then
+    depth = 0
+  else
+    depth = depth + 1
+    for i=1, depth do spaces = spaces .. "  " end
+  end
+  
+  if type(value) == 'table' then
+    mTable = getmetatable(value)
+    if mTable == nil then
+      print(spaces ..linePrefix.."(table) ")
+    else
+      print(spaces .."(metatable) ")
+        value = mTable
+    end		
+    for tableKey, tableValue in pairs(value) do
+      vardump(tableValue, depth, tableKey)
+    end
+  elseif type(value)	== 'function' or 
+      type(value)	== 'thread' or 
+      type(value)	== 'userdata' or
+      value		== nil
+  then
+    print(spaces..tostring(value))
+  else
+    print(spaces..linePrefix.."("..type(value)..") "..tostring(value))
+  end
+end
