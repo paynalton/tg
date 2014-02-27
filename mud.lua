9c307426a313e8a921cb1993b273694b3e5dfd04
paynalton <cxescalona@gmail.com>
2014-02-26 20:12:07 -0600

    testing lua file event

--- mud.lua
+++ mud.lua
@@ -49,26 +49,8 @@ function on_msg_receive (msg)
   if msg.out then
     return
   end
-  if (msg.text == 'ping') then
-    if (msg.to.id == our_id) then
-      print ('sending pong to ' .. tostring (msg.from.print_name))
-      send_msg (msg.from.print_name, 'pong')
-    else
-      print ('sending pong to ' .. tostring (msg.to.print_name))
-      send_msg (msg.to.print_name, 'pong')
-    end
-    return
-  end
-  if (msg.text == 'PING') then
-    if (msg.to.id == our_id) then
-      fwd_msg (msg.from.print_name, msg.id)
-    else
-      fwd_msg (msg.to.print_name, msg.id)
-    end
-    return
-  end
-  --vardump (msg)
-  --print ( "Message # " .. msg.id .. " (flags " .. msg.flags .. ")")
+  vardump (msg)
+  print ( "Message # " .. msg.id .. " (flags " .. msg.flags .. ")")
 end
 
 function on_our_id (id)
